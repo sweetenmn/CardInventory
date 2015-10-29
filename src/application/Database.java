fc7c8444350f08d155c4f87d3d6e102ec40282fd
Rmershon <MershonRB@hendrix.edu>
2015-10-29 16:36:01 -0500

    Attempted DB

--- src/application/Database.java
+++ src/application/Database.java
@@ -1,59 +1,70 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Database {
-	
-	public Database() throws ClassNotFoundException, InstantiationException, IllegalAccessException{
+
+	private int cardCount;
+	private int setCount;
+
+	public Database() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
 		System.out.println("here");
 		Class.forName("org.sqlite.JDBC");
-		
-		Connection connection = null;
-	
-		try
-		  {
-			  // create a database connection
-			  connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
-			  Statement statement = connection.createStatement();
-			  statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
-			  statement.executeUpdate("drop table if exists person");
-			  statement.executeUpdate("create table person (id integer, name string)");
-			  statement.executeUpdate("insert into person values(1, 'hans')");
-			  statement.executeUpdate("insert into person values(2, 'rebecca')");
-			  ResultSet rs = statement.executeQuery("select * from person");
-			  while(rs.next())
-			  {
-			    // read the result set
-			    System.out.println("name = " + rs.getString("name"));
-			    System.out.println("id = " + rs.getInt("id"));
-			  }
+		Connection connection = null;
+
+		try {
+			// create a database connection
+			connection = DriverManager.getConnection("jdbc:sqlite:CardInventory.db");
+			Statement statement = connection.createStatement();
+			statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+			DatabaseMetaData md = connection.getMetaData();
+			ResultSet rs = md.getTables(null, null, "%", null);
+
+			if (rs.getFetchSize() == 0) {
+				statement.executeUpdate("create table Set(SetId string, setName string)");
+				statement.executeUpdate("create table Card (CardId string, cardName string, setId string)");
+				statement.executeUpdate("create table Rarity (CardId string, Rarity string, Foil string)");
+				statement.executeUpdate("create table Condition (CardId string, NewMint integer, Excellent integer, " +
+						"VeryGood integer, Good integer, Poor integer)");
 			}
-			catch(SQLException e)
-			{
-			  // if the error message is "out of memory", 
-			  // it probably means no database file is found
-			  System.err.println(e.getMessage());
+		} catch (SQLException e) {
+			// if the error message is "out of memory",
+			// it probably means no database file is found
+			System.err.println(e.getMessage());
+		} finally {
+			try {
+				if (connection != null)
+					connection.close();
+			} catch (SQLException e) {
+				// connection close failed.
+				System.err.println(e);
 			}
-		finally
-		{
-		  try
-		  {
-		    if(connection != null)
-		      connection.close();
-		  }
-		  catch(SQLException e)
-		  {
-		    // connection close failed.
-		    System.err.println(e);
-		 }
+		}
+
 	}
-	
-	
+
+	public String addSet(String SetName) {
+		//TODO: assign the set a unique ID
+		//statement.executeUpdate("insert into Set values(
+		setCount += 1;
+
+		return("");
+
 	}
+
+	public String addCard(String CardName, String SetName) {
+		cardCount += 1;
+		return ("insert into Card values(");
 	}
+
+	public String getSet(String SetName) {
+		return("");
+	}
+
+	public String getCard(String CardName) {
+		return("");
+	}
+}
+
